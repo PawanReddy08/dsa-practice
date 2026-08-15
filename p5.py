2b1b39488a02a7d593f6ed20d662b91e3186c81c
Pawan <pawanreddyn26@gmail.com>
2026-08-15 21:01:19 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pattern5(self,n):
+        for i in range(n):
+            for j in range(n,i,-1):
+                print("* ",end=" ")
+            print()
+if __name__ == "__main__":
+    sol=Solution()
+    n=5
+    sol.pattern5(n)
