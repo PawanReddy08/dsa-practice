0bf16c6ec9e8af96dc9803ba7ac4ecc64017792b
Pawan <pawanreddyn26@gmail.com>
2026-08-17 20:28:47 +0530

    Add files via upload

---
+++ p13.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pattern13(self,n):
+        num=1
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                print(num,end=" ")
+                num+=1
+            print()
+if __name__ == "__main__":
+    sol=Solution()
+    n=5
+    sol.pattern13(n)  
