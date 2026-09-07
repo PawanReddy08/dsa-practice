da217a4023b39497011e78fce24ec83da87ba0e3
PawanReddy08 <pawanreddyn26@gmail.com>
2026-09-07 21:03:01 +0530

    prime

---
+++ BASIC_MATH/prime.py
@@ -0,0 +1,13 @@
+class solution():
+    def prime(self,n):
+        count=0
+        for i in range(1,n+1):
+            if n%i==0:
+                count+=1
+        if count==2:
+            return True
+        else:
+            return False
+obj=solution()
+n=19
+print(obj.prime(n))
